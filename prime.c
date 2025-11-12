3d5842370d2dc125445a1e3e3426ad73cad0b033
Vishwam2622 <vishwamkudekar1240@gmail.com>
2025-11-12 18:31:58 +0530

    To check Entered number is prime or not

--- prime.c
+++ prime.c
@@ -1,19 +1,20 @@
  #include <stdio.h>
     int main() {
-       int n, i, flag = 0;
-       printf("Enter a positive integer: ");
-       scanf("%d", &n);
-       if (n == 0 || n == 1)
-           flag = 1;
-       for (i = 2; i <= n / 2; ++i) {
-           if (n % i == 0) {
-               flag = 1;
-               break;
-           }
+       int a,b;
+       printf("Enter any number  ");
+       scanf("%d",&a);
+       if(a==2)
+       {
+        printf("It is a prime number");
+
        }
-       if (flag == 0)
-           printf("%d is a prime number.", n);
-       else
-           printf("%d is not a prime number.", n);
+       else if(a%2==0)
+       {
+        printf("It is not a prime number");
+       }
+       else{
+        printf("It is a prime number");
+       }
+
        return 0;
     }
